9cfa2916b1ffb463979435587ab6b0373cfd1a99
puspa shakya <84306848+Puspashakya@users.noreply.github.com>
2022-08-04 10:51:16 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,378 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width initial-scale=1.0">
+	<title>Bajracharya Store</title>
+	<link rel="stylesheet" href="style1.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;1,600&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+	<div class = "header">
+
+	<div class = "container">
+		<div class= "navbar">
+		<div class = "logo">
+			<a href="index.html"><img src = "images/logo.png" width="125px"></a>
+		</div>
+		<nav>
+			<ul id="MenuItems">
+				<li><a href="index.php">Home </a></li>
+				<li><a href="products.php">Products </a></li>
+				<li><a href="">About </a></li>
+				<li><a href="">Contact </a></li>
+				<li><a href="user_page.php">Login/Register </a></li>
+				<li><a href="Shopping_Cart\cart.php">cart</a></li>
+			</ul>
+		</nav>
+		<a href="\Shopping_Cart\cart.php"><img src="images/cart.png" width="30px" height="30px"> </a>
+		<img src="images/menu.png" class="menu-icon" onclick="menutoggle()">
+			</div>
+			<div class="row">
+				<div class="col-2">
+					<h1>Let's have a<br> new experince of shopping</h1>
+					<p>  hamro pasal ma tapai haru le saman kini<br> dinu vako ma dherai dherai swagat xa  </p>
+					<a href="" class="btn"> Explore Now &#8594; </a>
+				</div>
+				<div class="col-2">
+					<img src="images/image1.png">
+				</div>
+	</div>
+</div>
+  
+  <!--------------featured categories---------->
+  <div class="categories">
+  	<div class = "small-container">
+  		<div class="row">
+  		<div class="col-3">
+  			<img src="images/category-1.jpg">
+  		</div>
+  		<div class="col-3">
+  			<img src="images/category-2.jpg">
+  		</div>
+  		<div class="col-3">
+  			<img src="images/category-3.jpg">
+  		</div>
+  	</div>
+  	</div>
+</div>
+  		
+
+  		<!----------------Featured products ------------>
+  		<div class="small-container">
+  		<h2 class="title"> Featured Products 	</h2>
+  		<div class = "row">
+  			<div class = "col-4">
+  				<a href="product-detail.html"><img src="images/product-1.jpg"></a>
+  				<a href="product-detail.html"><h4> Red printed T-shirt </h4></a>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+	
+		<div class = "col-4">
+  				<img src="images/product-2.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+	
+
+		<div class = "col-4">
+  				<img src="images/product-3.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star-half-o"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+		<div class = "col-4">
+  				<img src="images/product-4.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+		</div>
+	
+	
+
+	<h2 class = "title"> Latest Products </h2>
+	<div class = "row">
+  			<div class = "col-4">
+  				<img src="images/product-5.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+	
+		<div class = "col-4">
+  				<img src="images/product-6.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+	
+
+		<div class = "col-4">
+  				<img src="images/product-7.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star-half-o"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+		<div class = "col-4">
+  				<img src="images/product-8.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+	
+
+		
+		
+		<div class = "row">
+  			<div class = "col-4">
+  				<img src="images/product-9.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+	
+		<div class = "col-4">
+  				<img src="images/product-10.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+		<div class = "col-4">
+  				<img src="images/product-11.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star-half-o"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+		<div class = "col-4">
+  				<img src="images/product-12.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+	</div>
+	
+
+	<!----------------------Offer---------------------->
+	<div class="offer">
+		<div class = "small-container">
+			<div class="row">
+				<div class= "col-2">
+					<img src="images/exclusive.png" class="offer-img">
+				</div>
+				<div class = "col-2">
+					<p>Exclusively Avaliable on RedStore</p>
+					<h1>Smart Band 4 </h1>
+					<small> Yo ekdum ramro product ho yo saman linu vaya ma ekdum faida hunxa  </small>
+					<a href="" class="btn">Buy now &#8594;</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+<!-------------testimonial ------------>
+<div class = "testimonial">
+	<div class="small-container">
+		<div class="row">
+			<div class="col-3">
+				<i class = "fa fa-quote-left"></i>
+				<p>  I recommend this app for the best user experiece  </p>
+				<div class ="rating">
+				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+				</div>
+				<img src="images/user-1.png">
+				<h3>Kul Man Ghising </h3>	
+			</div>
+			<div class="col-3">
+				<i class = "fa fa-quote-left"></i>
+				<p> Visit this site for the eye sight </p>
+				<div class ="rating">
+				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+				</div>
+				<img src="images/user-2.png">
+				<h3>Dr. Sanduk Ruit </h3>	
+			</div>
+			<div class="col-3">
+				<i class = "fa fa-quote-left"></i>
+				<p> National innovation center also visit this site </p>
+				<div class ="rating">
+				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+				</div>
+				<img src="images/user-3.png">
+				<h3>Maha Bir Pun </h3>	
+			</div>
+		</div>
+	</div>
+</div>					
+<!---------------------------------Brands--------------------->
+<div class="brands">
+	<div class = "small-container">
+		<div class = "row">
+			<div class="col-5">
+				<img src="images/e-sewa.jpg">
+			</div>
+			<div class="col-5">
+				<img src="images/khalti.jpg">
+			</div>
+			<div class="col-5">
+				<img src="images/fonepay.jpg">
+			</div>
+			<div class="col-5">
+				<img src="images/Ipay.jpg">
+			</div>
+			<div class="col-5">
+				<img src="images/prabhupay.jpg">
+			</div>
+		</div>
+	</div>
+</div>
+
+<!---------------------------------footer------------>
+  		<div class="footer">
+			<div class="container">
+				<div class="row">
+			<div class="footer-col-1">
+				<h3>Download our app </h3>
+				<p> Download app for android and ios mobile phone </p>
+				<div class="app-logo">
+					<img src="images/play-store.png">
+					<img src="images/app-store.png">
+				</div>
+				</div>
+			
+			<div class = "footer-col-2">
+				<img src="images/logo-white.png">
+				<p>  Hamro udesya vannu nai saral ra sahaj shopping exprerince garaunu ho  </p> 
+			</div>
+			<div class="footer-col-3">
+				<h3> UseFul links </h3>
+				<ul>
+					<li> Coupons </li>
+					<li> Blog Post </li>
+					<li> Return Policy </li>
+					<li> Join Affilate </li>
+				</ul>
+			</div>
+			<div class="footer-col-4">
+				<h3> Follow Us </h3>
+				<ul>
+					<li> Facebook </li>
+					<li> Twitter </li>
+					<li> Instagram </li>
+					<li> Youtube </li>
+				</ul>
+			</div>
+		
+		</div>
+		<hr> 
+		<p class="Copyright">Copyright 2022 </p> 
+	</div>
+</div>
+
+<!--------------Js for toggle menu------------->
+<script>
+	var MenuItems = document.getElementById("MenuItems");
+
+	MenuItems.sytle.maxHeight == "0px";
+
+	function menutoggle() {
+		if(MenuItems.style.maxHeight == "0px")
+		{
+			MenuItems.style.maxHeight = "200px";
+		}
+		else
+		{
+			MenuItems.style.maxHeight = "0px";
+		}
+	}
+</script>
+</body>
+</html>
---
+++ products.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width initial-scale=1.0">
+	<title>All products - Bajracharya store </title>
+	<link rel="stylesheet" href="style1.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;1,600&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+	
+
+	<div class = "container">
+		<div class= "navbar">
+		<div class = "logo">
+			<img src = "images/logo.png" width="125px">
+		</div>
+		<nav>
+			<ul id="MenuItems">
+				<li><a href="index.php">Home </a></li>
+				<li><a href="products.php">Products </a></li>
+				<li><a href="">About </a></li>
+				<li><a href="">Contact </a></li>
+				<li><a href="register_form.php">Account </a></li>
+			</ul>
+		</nav>
+		<img src="images/cart.png" width="30px" height="30px">
+		<img src="images/menu.png" class="menu-icon" onclick="menutoggle()">
+			</div>
+			
+	</div>
+
+  
+  		<div class="small-container">
+  		<div class = "row row-2">
+  			<h2> All product </h2>
+  			<select>
+<option> Default shorting </option>
+<option> Short by pricing </option>
+<option> Short by popularity </option>
+<option> Short by rating </option>
+<option> Short by sale </option>
+
+  			</select>
+  		</div>
+  		<div class = "row">
+  			<div class = "col-4">
+  				<img src="images/product-1.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+	
+		<div class = "col-4">
+  				<img src="images/product-2.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+	
+
+		<div class = "col-4">
+  				<img src="images/product-3.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star-half-o"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+		<div class = "col-4">
+  				<img src="images/product-4.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+		</div>
+	
+	
+
+	
+	<div class = "row">
+  			
+		<div class = "col-4">
+  				<img src="images/product-7.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star-half-o"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+		<div class = "col-4">
+  				<img src="images/product-8.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+	
+
+		
+		
+		<div class = "row">
+  			<div class = "col-4">
+  				<img src="images/product-9.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+	
+		<div class = "col-4">
+  				<img src="images/product-10.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+		<div class = "col-4">
+  				<img src="images/product-11.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star-half-o"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+		<div class = "col-4">
+  				<img src="images/product-12.jpg">
+  				<h4> Red printed T-shirt </h4>
+  				<div class= "rating"> 
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+			</div>	
+			<p> $50.00 </p>
+		</div>
+	</div>
+	
+
+	<!----------------------Offer---------------------->
+	<div class="offer">
+		<div class = "small-container">
+			<div class="row">
+				<div class= "col-2">
+					<img src="images/exclusive.png" class="offer-img">
+				</div>
+				<div class = "col-2">
+					<p>Exclusively Avaliable on RedStore</p>
+					<h1>Smart Band 4 </h1>
+					<small> Yo ekdum ramro product ho yo saman linu vaya ma ekdum faida hunxa  </small>
+					<a href="" class="btn">Buy now &#8594;</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+<!-------------testimonial ------------>
+<div class = "testimonial">
+	<div class="small-container">
+		<div class="row">
+			<div class="col-3">
+				<i class = "fa fa-quote-left"></i>
+				<p> ljasoijoiejoij ljifs lisjf ojfi lojf ojif oijf </p>
+				<div class ="rating">
+				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+				</div>
+				<img src="images/user-1.png">
+				<h3>Sean Parker </h3>	
+			</div>
+			<div class="col-3">
+				<i class = "fa fa-quote-left"></i>
+				<p> ljasoijoiejoij ljifs lisjf ojfi lojf ojif oijf </p>
+				<div class ="rating">
+				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+				</div>
+				<img src="images/user-2.png">
+				<h3>Mike Smith </h3>	
+			</div>
+			<div class="col-3">
+				<i class = "fa fa-quote-left"></i>
+				<p> ljasoijoiejoij ljifs lisjf ojfi lojf ojif oijf </p>
+				<div class ="rating">
+				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+  				<i class="fa fa-star"></i>
+				<i class="fa fa-star-o"></i>
+				</div>
+				<img src="images/user-3.png">
+				<h3>Mabel Joe </h3>	
+			</div>
+		</div>
+	</div>
+</div>					
+<!---------------------------------Brands--------------------->
+<div class="brands">
+	<div class = "small-container">
+		<div class = "row">
+			<div class="col-5">
+				<img src="images/e-sewa.jpg">
+			</div>
+			<div class="col-5">
+				<img src="images/khalti.jpg">
+			</div>
+			<div class="col-5">
+				<img src="images/fonepay.jpg">
+			</div>
+			<div class="col-5">
+				<img src="images/Ipay.jpg">
+			</div>
+			<div class="col-5">
+				<img src="images/prabhupay.jpg">
+			</div>
+		</div>
+	</div>
+
+	<div class="page-btn">
+		<h2> All product <h2>
+			<span>1</span>
+			<span>2</span>
+			<span>3</span>
+			<span>4</span>
+			<span>5</span>
+			<span>&#8594; </span>
+	</div>
+</div>
+
+<!---------------------------------footer------------>
+  		<div class="footer">
+			<div class="container">
+				<div class="row">
+			<div class="footer-col-1">
+				<h3>Download our app </h3>
+				<p> Download app for android and ios mobile phone </p>
+				<div class="app-logo">
+					<img src="images/play-store.png">
+					<img src="images/app-store.png">
+				</div>
+				</div>
+			
+			<div class = "footer-col-2">
+				<img src="images/logo-white.png">
+				<p>  Hamro udesya vannu nai saral ra sahaj shopping exprerince garaunu ho  </p> 
+			</div>
+			<div class="footer-col-3">
+				<h3> UseFul links </h3>
+				<ul>
+					<li> Coupons </li>
+					<li> Blog Post </li>
+					<li> Return Policy </li>
+					<li> Join Affilate </li>
+				</ul>
+			</div>
+			<div class="footer-col-4">
+				<h3> Follow Us </h3>
+				<ul>
+					<li> Facebook </li>
+					<li> Twitter </li>
+					<li> Instagram </li>
+					<li> Youtube </li>
+				</ul>
+			</div>
+		
+		</div>
+		<hr> 
+		<p class="Copyright">Copyright 2022 </p> 
+	</div>
+</div>
+
+<!--------------Js for toggle menu------------->
+<script>
+	var MenuItems = document.getElementById("MenuItems");
+
+	MenuItems.sytle.maxHeight == "0px";
+
+	function menutoggle() {
+		if(MenuItems.style.maxHeight == "0px")
+		{
+			MenuItems.style.maxHeight = "200px";
+		}
+		else
+		{
+			MenuItems.style.maxHeight = "0px";
+		}
+	}
+</script>
+</body>
+</html>
